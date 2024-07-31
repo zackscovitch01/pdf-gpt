0e3fbdc827494c4fbf44ef3f1cde8d092b8eb392
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-07-31 18:48:53 -0400

    landing page

---
+++ constants/index.ts
@@ -0,0 +1,46 @@
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
+
+export const features = [
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your import PDF files securely stored and easily accessible anytime anywhere",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Fast Responses",
+    description: "Get instant responses to your queries and issues",
+    icon: ZapIcon,
+  },
+  {
+    name: "Chat Memorisation",
+    description:
+      "Chat with our AI and it will remember your previous conversations ensure a seamless and personal experience",
+    icon: BrainCogIcon,
+  },
+  {
+    name: "Interactive PDF Viewer",
+    description:
+      "View your PDF files in an interactive and user-friendly interface",
+    icon: EyeIcon,
+  },
+  {
+    name: "Cloud Backup",
+    description:
+      "All your data is backed up on the cloud ensuring you never lose your important documents",
+    icon: ServerCogIcon,
+  },
+  {
+    name: "Responsive Design",
+    description:
+      "Our app is designed to work on all devices, be it a desktop, tablet or mobile",
+    icon: MonitorSmartphoneIcon,
+  },
+];
