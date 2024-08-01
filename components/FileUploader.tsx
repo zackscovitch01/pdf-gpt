e5ba365485fad484d7eb6c41d3db7f18ef99681d
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-07-31 21:43:52 -0400

    .

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -35,7 +35,7 @@ function FileUploader() {
           ) : (
             <>
               <CircleArrowDown className="h-20 w-20 animate-bounce" />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <p>Drag {"'n'"} drop some files here, or click to select files</p>
             </>
           )}
         </div>
