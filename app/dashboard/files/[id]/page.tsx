f7325caa5edf781090bf39fe76544a46b4f0a1ff
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-08-02 06:32:44 -0400

    PDFView done

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,10 +1,32 @@
-function ChatToFilePage({
+import PdfView from "@/components/PdfView";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+
+async function ChatToFilePage({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) {
-  return <div>ChatToFilePage: {id}</div>;
+  auth().protect();
+  const { userId } = await auth();
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+  const url = ref.data()?.downloadUrl;
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* Right */}
+      <div className="lg:col-span-2 overflow-y-auto">{/* Chat */}</div>
+      {/* Left */}
+      <div className="lg:col-span-3 bg-gray-100 lg:border-r-2 lg:border-pink-600 lg:-order-1 overflow-auto">
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 export default ChatToFilePage;
