48a1d32e25d968610ad3f9417f644183764d4163
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-08-08 05:24:02 -0400

    download functionality

--- components/Document.tsx
+++ components/Document.tsx
@@ -2,6 +2,10 @@
 
 import { useRouter } from "next/navigation";
 import byteSize from "byte-size";
+import useSubscription from "@/hooks/useSubscription";
+import { useTransition } from "react";
+import { DownloadCloud, Trash2Icon } from "lucide-react";
+import { Button } from "./ui/button";
 
 function Document({
   name,
@@ -15,6 +19,8 @@ function Document({
   id: string;
 }) {
   const router = useRouter();
+  const [isDeleting, startTransition] = useTransition();
+  const { hasActiveMembership } = useSubscription();
 
   return (
     <div className="flex flex-col w-64 h-80 rounded-xl bg-white drop-shadow-md justify-between p-4 transition-all transform hover:scale-105 hover:bg-pink-600 hover:text-white cursor-pointer group">
@@ -32,6 +38,14 @@ function Document({
           {/* render size in kbs */}
           {byteSize(size).value} KB
         </p>
+      </div>
+      {/* Actions */}
+      <div className="flex space-x-2 justify-end">
+        <Button variant="ghost" asChild>
+          <a href={downloadUrl} download>
+            <DownloadCloud className="h-6 w-6 text-pink-600" />
+          </a>
+        </Button>
       </div>
     </div>
   );
